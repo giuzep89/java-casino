5a9a897df87a440fdaeceff81a8b79801db3ede8
giuzep89 <g.rinaldimusic@gmail.com>
2025-08-07 14:26:07 +0200

    higher-lower game written

--- novi/higherlower/HigherLowerGame.java
+++ novi/higherlower/HigherLowerGame.java
@@ -12,16 +12,27 @@ public class HigherLowerGame {
 
     public void playGame() {
         gameIsRunning = true;
+        int turns = 0;
 
         // Here you should generate the number to guess
+        int numberToGuess = (int)Math.round(Math.random() * 100);
 
         while (gameIsRunning) {
             System.out.println("Make a guess");
-
             var input = inputScanner.nextLine();
             System.out.printf("You typed: %s\n", input);
+            turns++;
 
             // Check if the guess is correct, if so end the game
+            if (Integer.parseInt(input) > numberToGuess){
+                System.out.println("That number is too high!");
+            } else if (Integer.parseInt(input) < numberToGuess){
+                System.out.println("That number is too low!");
+            } else if (Integer.parseInt(input) == numberToGuess){
+                System.out.printf("Correct! You guessed the number in %s turns", turns);
+                gameIsRunning = false;
+            }
+
         }
     }
 }
